215f92a7d86a383df39a1a4c7698ca9addaf65dc
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-02-09 17:56:54 +0200

    feat: add antd + react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import {Dashboard} from './components';
+import {lazy, Suspense} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Dashboard = lazy(() => import('./components/Dashboard/Dashboard'));
+
+const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+`;
 
 const App = () => {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <AppContainer>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
+      </AppContainer>
+    </BrowserRouter>
   );
 };
 
