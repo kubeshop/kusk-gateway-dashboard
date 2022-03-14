191d9330481f1534c4ffef7974c40aad9943a29e
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-14 19:09:02 +0200

    feat: collapse all operations functionality

--- src/swaggerUI/plugins/CollapseOperationsPlugin/CollapseOperationsPlugin.tsx
+++ src/swaggerUI/plugins/CollapseOperationsPlugin/CollapseOperationsPlugin.tsx
@@ -1,13 +1,35 @@
+import {getOperationId} from '@swaggerUI/utils/operations';
+
 import * as S from './styled';
 
-const CollapseOperationsPlugin = (system: any) => ({
+const CollapseOperationsPlugin = () => ({
   wrapComponents: {
     operations: (Original: any) => (props: any) => {
+      const {layoutActions, layoutSelectors, specSelectors} = props;
+
+      const closeAllOperationsHandler = () => {
+        const operations = specSelectors.operations().toJS();
+        operations.forEach((op: any) => {
+          const {method, path, operation} = op;
+
+          const operationId = getOperationId(path, method, operation);
+          const tags = operation.tags || ['default'];
+
+          tags.forEach((tag: string) => {
+            const isShownKey = ['operations', tag, operationId];
+
+            if (layoutSelectors.isShown(isShownKey)) {
+              layoutActions.show(isShownKey, false);
+            }
+          });
+        });
+      };
+
       return (
         <S.CollapseExpandOperationsPluginContainer>
           <Original {...props} />
 
-          <S.CollapseOperationsButton>Collapse all</S.CollapseOperationsButton>
+          <S.CollapseOperationsButton onClick={closeAllOperationsHandler}>Collapse all</S.CollapseOperationsButton>
         </S.CollapseExpandOperationsPluginContainer>
       );
     },
