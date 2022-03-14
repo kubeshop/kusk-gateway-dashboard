a8860cc079df3d51f76e402791b8404b84fe9606
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-14 17:24:03 +0200

    feat: get raw open api spec for kusk extensions

--- src/components/KuskExtensions/KuskExtensions.tsx
+++ src/components/KuskExtensions/KuskExtensions.tsx
@@ -1,5 +1,27 @@
+import {Skeleton} from 'antd';
+
+import {useGetRawOpenApiSpec} from '@models/api';
+
+import {useAppSelector} from '@redux/hooks';
+
+import * as S from './styled';
+
 const KuskExtensions: React.FC = () => {
-  return <div>Kusk Extensions</div>;
+  const selectedApi = useAppSelector(state => state.main.selectedApi);
+
+  const {data, loading, error} = useGetRawOpenApiSpec({apiId: selectedApi});
+
+  return (
+    <S.KuskExtensionsContainer>
+      {loading ? (
+        <Skeleton />
+      ) : error ? (
+        <S.ErrorLabel>{error.message}</S.ErrorLabel>
+      ) : (
+        data && <div>Kusk Extensions</div>
+      )}
+    </S.KuskExtensionsContainer>
+  );
 };
 
 export default KuskExtensions;
---
+++ src/components/KuskExtensions/styled.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ErrorLabel = styled.span`
+  color: red;
+`;
+
+export const KuskExtensionsContainer = styled.div``;
