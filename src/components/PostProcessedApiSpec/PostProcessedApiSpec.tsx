369d5539ed691cdec9cd040a6575fac43fd110fe
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-23 16:10:32 +0200

    feat: remove kusk extensions from spec

--- src/components/PostProcessedApiSpec/PostProcessedApiSpec.tsx
+++ src/components/PostProcessedApiSpec/PostProcessedApiSpec.tsx
@@ -26,10 +26,32 @@ const PostProcessedApiSpec: React.FC = () => {
       ) : error ? (
         <S.ErrorLabel>{error.message}</S.ErrorLabel>
       ) : (
-        data && <SwaggerUI spec={YAML.parse(data)} plugins={[TableOfContentsPlugin, DynamicServersPlugin]} />
+        data && (
+          <SwaggerUI
+            spec={removeKuskExtensions(YAML.parse(data))}
+            plugins={[TableOfContentsPlugin, DynamicServersPlugin]}
+          />
+        )
       )}
     </S.PostProcessedApiSpecContainer>
   );
+};
+
+const removeKuskExtensions = (spec: any) => {
+  const parsedSpec = spec;
+
+  // delete top-level x-kusk extension
+  delete parsedSpec['x-kusk'];
+
+  Object.values(spec).forEach(val => {
+    if (typeof val !== 'object') {
+      return;
+    }
+
+    removeKuskExtensions(val);
+  });
+
+  return parsedSpec;
 };
 
 export default PostProcessedApiSpec;
