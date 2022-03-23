0b53805f8db0b14765656fb3a3c72fc1bd93874c
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-23 17:28:45 +0200

    feat: parse post-processed spec

--- src/components/PostProcessedApiSpec/PostProcessedApiSpec.tsx
+++ src/components/PostProcessedApiSpec/PostProcessedApiSpec.tsx
@@ -3,7 +3,7 @@ import {Skeleton} from 'antd';
 import SwaggerUI from 'swagger-ui-react';
 import YAML from 'yaml';
 
-import {useGetPostProcessedOpenApiSpec} from '@models/api';
+import {useGetRawOpenApiSpec} from '@models/api';
 
 import {useAppSelector} from '@redux/hooks';
 
@@ -14,7 +14,8 @@ import * as S from './styled';
 const PostProcessedApiSpec: React.FC = () => {
   const selectedApi = useAppSelector(state => state.main.selectedApi);
 
-  const {data, error, loading} = useGetPostProcessedOpenApiSpec({
+  // TODO: use postProcessed endpoint
+  const {data, error, loading} = useGetRawOpenApiSpec({
     name: selectedApi?.name || '',
     namespace: selectedApi?.namespace || '',
   });
@@ -26,32 +27,48 @@ const PostProcessedApiSpec: React.FC = () => {
       ) : error ? (
         <S.ErrorLabel>{error.message}</S.ErrorLabel>
       ) : (
-        data && (
-          <SwaggerUI
-            spec={removeKuskExtensions(YAML.parse(data))}
-            plugins={[TableOfContentsPlugin, DynamicServersPlugin]}
-          />
-        )
+        data && <SwaggerUI spec={parseSpec(YAML.parse(data))} plugins={[TableOfContentsPlugin, DynamicServersPlugin]} />
       )}
     </S.PostProcessedApiSpecContainer>
   );
 };
 
-const removeKuskExtensions = (spec: any) => {
-  const parsedSpec = spec;
+const parseSpec = (spec: any) => {
+  const topLevelDisabled = spec['x-kusk']?.disabled;
 
-  // delete top-level x-kusk extension
-  delete parsedSpec['x-kusk'];
+  delete spec['x-kusk'];
 
-  Object.values(spec).forEach(val => {
-    if (typeof val !== 'object') {
-      return;
+  // path level
+  Object.entries(spec.paths).forEach((pathEntry: [string, any]) => {
+    const [path, pathValue] = pathEntry;
+
+    const pathDisabled = pathValue['x-kusk']?.disabled;
+
+    delete spec.paths[path]['x-kusk'];
+
+    // method level
+    Object.entries(pathValue).forEach((methodEntry: [string, any]) => {
+      const [method, methodValue] = methodEntry;
+
+      // check if top-level or path level disabled
+      if (topLevelDisabled || pathDisabled || methodValue['x-kusk']?.disabled) {
+        // check if disabled is not set to false on path or operation level
+        if (pathDisabled !== false && methodValue['x-kusk']?.disabled !== false) {
+          delete spec.paths[path][method];
+        }
+      }
+
+      // delete x-kusk extension for operation level if still exists
+      if (spec.paths[path][method]) {
+        delete spec.paths[path][method]['x-kusk'];
+      }
+    });
+
+    if (!Object.keys(spec.paths[path]).length) {
+      delete spec.paths[path];
     }
-
-    removeKuskExtensions(val);
   });
-
-  return parsedSpec;
+  return spec;
 };
 
 export default PostProcessedApiSpec;
