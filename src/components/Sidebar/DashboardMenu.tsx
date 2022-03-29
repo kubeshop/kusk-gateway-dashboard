ebb15ec85a4200554548458c4b394dcf825b9ef5
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-29 13:34:53 +0300

    feat: add menu icon tooltip

--- src/components/Sidebar/DashboardMenu.tsx
+++ src/components/Sidebar/DashboardMenu.tsx
@@ -1,5 +1,9 @@
 import {Link, useLocation} from 'react-router-dom';
 
+import {Tooltip} from 'antd';
+
+import {TOOLTIP_DELAY} from '@constants/constants';
+
 import {Api, EnvoyFleet, StaticRoute} from '@components/Icons';
 
 import * as S from './DashboardMenu.styled';
@@ -9,17 +13,23 @@ const DashboardMenu: React.FC = () => {
 
   return (
     <S.DashboardMenuContainer>
-      <Link to="/">
-        <S.Icon $border component={Api} $active={pathname === '/'} />
-      </Link>
+      <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="APIs List">
+        <Link to="/">
+          <S.Icon $border component={Api} $active={pathname === '/'} />
+        </Link>
+      </Tooltip>
 
-      <Link to="/envoy-fleets">
-        <S.Icon $border component={EnvoyFleet} $active={pathname === '/envoy-fleets'} />
-      </Link>
+      <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="EnvoyFleets List">
+        <Link to="/envoy-fleets">
+          <S.Icon $border component={EnvoyFleet} $active={pathname === '/envoy-fleets'} />
+        </Link>
+      </Tooltip>
 
-      <Link to="/static-routes">
-        <S.Icon component={StaticRoute} $active={pathname === '/static-routes'} />
-      </Link>
+      <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="StaticRoutes List">
+        <Link to="/static-routes">
+          <S.Icon component={StaticRoute} $active={pathname === '/static-routes'} />
+        </Link>{' '}
+      </Tooltip>
     </S.DashboardMenuContainer>
   );
 };
