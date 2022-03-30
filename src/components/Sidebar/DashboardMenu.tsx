846a408d9fc062f3b8d00b5fcbfa03b269cc0e5f
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-03-30 09:48:52 +0300

    refactor: rename dashboard menu icons components

--- src/components/Sidebar/DashboardMenu.tsx
+++ src/components/Sidebar/DashboardMenu.tsx
@@ -4,7 +4,7 @@ import {Tooltip} from 'antd';
 
 import {TOOLTIP_DELAY} from '@constants/constants';
 
-import {Api, EnvoyFleet, StaticRoute} from '@components/Icons';
+import {ApiIcon, EnvoyFleetIcon, StaticRouteIcon} from '@components/Icons';
 
 import * as S from './DashboardMenu.styled';
 
@@ -15,19 +15,19 @@ const DashboardMenu: React.FC = () => {
     <S.DashboardMenuContainer>
       <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="APIs List">
         <Link to="/">
-          <S.Icon $border component={Api} $active={pathname === '/'} />
+          <S.Icon $border component={ApiIcon} $active={pathname === '/'} />
         </Link>
       </Tooltip>
 
       <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="EnvoyFleets List">
         <Link to="/envoy-fleets">
-          <S.Icon $border component={EnvoyFleet} $active={pathname === '/envoy-fleets'} />
+          <S.Icon $border component={EnvoyFleetIcon} $active={pathname === '/envoy-fleets'} />
         </Link>
       </Tooltip>
 
       <Tooltip mouseEnterDelay={TOOLTIP_DELAY} title="StaticRoutes List">
         <Link to="/static-routes">
-          <S.Icon component={StaticRoute} $active={pathname === '/static-routes'} />
+          <S.Icon component={StaticRouteIcon} $active={pathname === '/static-routes'} />
         </Link>
       </Tooltip>
     </S.DashboardMenuContainer>
