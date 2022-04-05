4b07b8e0628f46fb93af00013393c99b8b32073e
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-04-05 14:35:48 +0300

    feat: modal api content step + validation

--- src/components/ApiDeployModal/ApiDeployModal.tsx
+++ src/components/ApiDeployModal/ApiDeployModal.tsx
@@ -1,4 +1,8 @@
-import {Modal} from 'antd';
+import {useState} from 'react';
+
+import {Form, Modal, Steps} from 'antd';
+
+import YAML from 'yaml';
 
 import {useAppDispatch} from '@redux/hooks';
 import {closeApiDeployModal} from '@redux/reducers/ui';
@@ -8,11 +12,85 @@ import * as S from './styled';
 const ApiDeployModal: React.FC = () => {
   const dispatch = useAppDispatch();
 
-  const onCancelHandler = () => dispatch(closeApiDeployModal());
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const [apiContent, setApiContent] = useState<{[key: string]: any}>();
+
+  const [form] = Form.useForm();
+
+  const onCancelHandler = () => {
+    if (activeStep) {
+      setActiveStep(0);
+    } else {
+      dispatch(closeApiDeployModal());
+    }
+  };
+
+  const onOkHandler = () => {
+    if (activeStep) {
+      form.validateFields().then(() => {
+        console.log(apiContent);
+      });
+
+      //   const apiContent = YAML.parse(JSON.parse(JSON.stringify(values.content)));
+
+      //   console.log(apiContent);
+    } else {
+      form.validateFields().then(values => {
+        setApiContent(YAML.parse(JSON.parse(JSON.stringify(values.content))));
+        setActiveStep(1);
+      });
+    }
+  };
 
   return (
-    <Modal visible title="Deploy New API" width="800px" onCancel={onCancelHandler}>
-      <S.Container>Test</S.Container>
+    <Modal
+      cancelText={activeStep ? 'Back' : 'Cancel'}
+      title="Deploy New API"
+      visible
+      width="800px"
+      onCancel={onCancelHandler}
+      okText={activeStep ? 'Deploy' : 'Next'}
+      onOk={onOkHandler}
+    >
+      <S.Container>
+        <S.StepsContainer>
+          <Steps direction="vertical" current={activeStep}>
+            <S.Step title="API Content" />
+            <S.Step title="Upstream Service" />
+          </Steps>
+        </S.StepsContainer>
+
+        <Form form={form} layout="vertical">
+          {activeStep === 0 ? (
+            <Form.Item
+              name="content"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter your API content!',
+                },
+                () => {
+                  return {
+                    validator(_, value) {
+                      if (typeof YAML.parse(JSON.parse(JSON.stringify(value))) === 'object') {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject(new Error('Please enter a valid API content!'));
+                    },
+                  };
+                },
+              ]}
+            >
+              <S.Textarea rows={20} placeholder="Enter API content in YAML/JSON format" />
+            </Form.Item>
+          ) : (
+            <Form.Item name="test">
+              <S.Textarea />
+            </Form.Item>
+          )}
+        </Form>
+      </S.Container>
     </Modal>
   );
 };
--- src/components/ApiDeployModal/styled.tsx
+++ src/components/ApiDeployModal/styled.tsx
@@ -1,7 +1,41 @@
+import {Input, Steps} from 'antd';
+
 import styled from 'styled-components';
+
+import {GlobalScrollbarStyle} from '@utils/scrollbar';
+
+import Colors from '@styles/colors';
 
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 200px 1fr;
   grid-column-gap: 10px;
+`;
+
+export const Step = styled(Steps.Step)`
+  & .ant-steps-icon {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & .ant-steps-item-title {
+    width: 155px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+`;
+
+export const StepsContainer = styled.div`
+  padding-right: 5px;
+  max-height: 500px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  ${GlobalScrollbarStyle};
+`;
+
+export const Textarea = styled(Input.TextArea)`
+  background-color: ${Colors.grey2};
 `;
