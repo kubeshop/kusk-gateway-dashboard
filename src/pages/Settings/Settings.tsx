e2355b5d372b61883573af35c84c1641c474fa05
Topliceanu Razvan <topliceanurazvan98@gmail.com>
2022-04-28 11:43:49 +0300

    feat: Settings API endpoint input

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,7 +1,31 @@
+import {Button, Form, Input} from 'antd';
+
+import {ContentWrapper} from '@components/AntdCustom';
+
 import * as S from './styled';
 
 const Settings: React.FC = () => {
-  return <S.SettingsContainer>Settings</S.SettingsContainer>;
+  const [form] = Form.useForm();
+
+  const onFinishHandler = (values: any) => {
+    const {apiEndpoint} = values;
+
+    console.log(apiEndpoint);
+  };
+
+  return (
+    <ContentWrapper>
+      <S.Form form={form} layout="vertical" onFinish={onFinishHandler}>
+        <Form.Item label="API Endpoint" name="apiEndpoint">
+          <Input placeholder="Enter API endpoint" type="text" />
+        </Form.Item>
+
+        <Button htmlType="submit" type="primary">
+          Apply
+        </Button>
+      </S.Form>
+    </ContentWrapper>
+  );
 };
 
 export default Settings;
