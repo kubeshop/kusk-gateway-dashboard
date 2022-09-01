65d0b776d23682e4488fd085a49d80a43f237e98
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-09-01 09:43:30 +0200

    fix: Monaco for crd

--- src/components/Monaco/Monaco.styled.tsx
+++ src/components/Monaco/Monaco.styled.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 import Colors from '@styles/colors';
 
-export const MonacoContainer = styled.div`
+export const MonacoContainer = styled.div<{$fullWidth?: boolean}>`
   padding-left: 0px;
   padding-right: 8px;
   margin: 0px;
   border-radius: 4px;
   display: flex;
   min-height: calc(100% - 118px);
-  width: 49%;
+  width: ${({$fullWidth}) => ($fullWidth ? '100%' : '49%')};
   border: 1px solid ${Colors.zinc2};
   background: ${Colors.whitePure};
 
