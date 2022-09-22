2b76e24a1be9d7a16545e79e15fba4467be9a01e
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-09-22 15:34:51 +0200

    fix: remove static routes from settings

--- src/components/Settings/StaticRoutesSettings/styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div``;
-
-export const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 32px;
-`;
