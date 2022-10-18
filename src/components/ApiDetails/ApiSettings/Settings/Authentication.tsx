ae8e3a54c50306920d286b9ab106a8f6d07a999e
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-10-18 13:57:45 +0200

    fix: remove scheme

--- src/components/ApiDetails/ApiSettings/Settings/Authentication.tsx
+++ src/components/ApiDetails/ApiSettings/Settings/Authentication.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {useDispatch} from 'react-redux';
 
 import {Form, Input, InputNumber, Select, Switch} from 'antd';
@@ -11,7 +12,8 @@ import * as S from './styled';
 
 const Authentication = () => {
   const [form] = Form.useForm();
-  const authSchema = Form.useWatch(['x-kusk', 'auth', 'scheme'], form);
+  const authEnabled = Form.useWatch('enabled', form);
+  const [authSchema, setAuthScheme] = useState<string | undefined>('custom');
   const dispatch = useDispatch();
   const selectedAPIOpenSpec = useAppSelector(state => state.main.selectedApiOpenapiSpec);
   const xKusk = selectedAPIOpenSpec && selectedAPIOpenSpec['x-kusk'];
@@ -19,11 +21,14 @@ const Authentication = () => {
   const onSaveClickHandler = (values: any) => {
     const {enabled, ...rest} = values;
     if (enabled) {
-      delete rest['x-kusk'].auth.scheme;
       dispatch(updateApiSettings({editedOpenapi: rest}));
     } else {
       dispatch(updateApiSettings({editedOpenapi: {'x-kusk': {auth: null}}}));
     }
+  };
+
+  const onSelectSchemeHandler = (value: string) => {
+    setAuthScheme(value);
   };
   return (
     <FormCard
@@ -33,67 +38,69 @@ const Authentication = () => {
       helpLink="https://docs.kusk.io/extension#authentication"
       cardProps={{
         extra: (
-          <Form.Item label="Enable" name={['enabled']} valuePropName="checked" initialValue={Boolean(xKusk?.auth)}>
+          <Form.Item label="Enable" name="enabled" valuePropName="checked" initialValue={Boolean(xKusk?.auth)}>
             <Switch />
           </Form.Item>
         ),
       }}
-      formProps={{layout: 'vertical', onFinish: onSaveClickHandler, form}}
+      formProps={{form, layout: 'vertical', onFinish: onSaveClickHandler}}
     >
-      <Form.Item
-        label="Authentication Scheme"
-        name={['x-kusk', 'auth', 'scheme']}
-        initialValue={Object.keys(xKusk?.auth || [])[0]}
-      >
-        <Select>
+      <Form.Item label="Authentication Scheme">
+        <Select onChange={onSelectSchemeHandler} value={Object.keys(xKusk?.auth || [])[0]} disabled={!authEnabled}>
           <Select.Option value="custom">Custom</Select.Option>
         </Select>
       </Form.Item>
 
-      <Form.Item
-        label="Path Prefix"
-        name={['x-kusk', 'auth', authSchema, 'path_prefix']}
-        initialValue={xKusk?.auth?.custom?.path_prefix}
-      >
-        <Input placeholder="e.g. /login" />
-      </Form.Item>
-
-      <S.Row>
-        <S.CardItem style={{flex: 1}}>
+      {authSchema === 'custom' && (
+        <>
           <Form.Item
-            label="Hostname"
-            name={['x-kusk', 'auth', authSchema, 'host', 'hostname']}
-            initialValue={xKusk?.auth.custom?.host?.hostname}
-            rules={[
-              {
-                required: true,
-                message: 'Please enter hostname',
-              },
-            ]}
+            shouldUpdate={(prevValues, curValues) => prevValues.enabled !== curValues.enabled}
+            label="Path Prefix"
+            name={['x-kusk', 'auth', 'custom', 'path_prefix']}
+            initialValue={xKusk?.auth?.custom?.path_prefix}
           >
-            <Input placeholder="e.g. example.com" />
+            <Input placeholder="e.g. /login" disabled={!authEnabled} />
           </Form.Item>
-        </S.CardItem>
-        <Form.Item
-          label="Port"
-          name={['x-kusk', 'auth', authSchema, 'host', 'port']}
-          initialValue={xKusk?.auth?.custom?.host?.port}
-          rules={[
-            {
-              required: true,
-              message: 'Please enter port',
-            },
-          ]}
-        >
-          <InputNumber
-            style={{minWidth: '100%'}}
-            placeholder="Target port to which requests should be routed"
-            type="number"
-          />
-        </Form.Item>
-      </S.Row>
 
-      <S.Divider />
+          <S.Row>
+            <S.CardItem style={{flex: 1}}>
+              <Form.Item
+                label="Hostname"
+                name={['x-kusk', 'auth', 'custom', 'host', 'hostname']}
+                initialValue={xKusk?.auth.custom?.host?.hostname}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please enter hostname',
+                  },
+                ]}
+              >
+                <Input placeholder="e.g. example.com" disabled={!authEnabled} />
+              </Form.Item>
+            </S.CardItem>
+            <Form.Item
+              label="Port"
+              name={['x-kusk', 'auth', 'custom', 'host', 'port']}
+              initialValue={xKusk?.auth?.custom?.host?.port}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter port',
+                },
+              ]}
+            >
+              <InputNumber
+                style={{minWidth: '100%'}}
+                placeholder="Target port to which requests should be routed"
+                type="number"
+                disabled={!authEnabled}
+              />
+            </Form.Item>
+          </S.Row>
+
+          <S.Divider />
+        </>
+      )}
     </FormCard>
   );
 };
