68d5ce820d75947991b25b8b81fc8bee7ed36e9e
Mahmoud Mortada <mortda.m.11@gmail.com>
2022-10-26 13:44:40 +0200

    fix: policies form

--- src/components/Policies/index.ts
+++ src/components/Policies/index.ts
@@ -4,6 +4,5 @@ export {default as TargetsPolicy} from './Targets';
 export {default as ValidationPolicy} from './Validation';
 export {default as QOSPolicy} from './QOS';
 export {default as AuthenticationPolicy} from './Authentication';
-export {default as MockingPolicy} from './Mocking';
 export {default as CachingPolicy} from './Caching';
 export {default as WebsocketPolicy} from './Websocket';
